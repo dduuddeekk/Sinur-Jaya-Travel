7c17cf932f935fa28025effabb33fdb6f74554ec
dduuddeekk <jajajablablabla123@gmail.com>
2023-06-11 16:05:51 +0800

    Update Bus.

--- php/getbus.php
+++ php/getbus.php
@@ -1,3 +1,37 @@
 <?php
     include "../php/connect.php";
+
+    $collection = $database->selectCollection("bus");
+
+    $id = isset($_POST["id"]) ? $_POST["id"] : "";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/updatebus.css">
+    <title>PERBARUI BUS</title>
+</head>
+<body>
+    <h1>PERBARUI BUS</h1>
+    <form action="../php/updatebus.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <label for="plat">Nomor STNK:</label>
+        <label for="photo">Foto:</label>
+        <input type="file" name="image" />
+        <input type="text" name="plat">
+        <label for="type">Jenis:</label>
+        <select name="type">
+            <option value="BIG">BIG</option>
+            <option value="MEDIUM">MEDIUM</option>
+            <option value="MICRO">MICRO</option>
+        </select>
+        <label for="chair">Kursi:</label>
+        <input type="number" name="chair">
+        <button type="submit">PERBARUI</button>
+        <a href="javascript:history.back()" class="button-cancel">BATAL</a>
+    </form>
+</body>
+</html>
---
+++ php/updatebus.php
@@ -0,0 +1,37 @@
+<?php
+    include "../php/connect.php";
+
+    $collection = $database->selectCollection("bus");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id = $_POST["id"];
+        $image = $_FILES["image"];
+        $plat = isset($_POST["plat"]) ? $_POST["plat"] : "";
+        $jenis = isset($_POST["type"]) ? $_POST["type"] : ""; // Fix: Change "jenis" to "type"
+        $kursi = isset($_POST["chair"]) ? intval($_POST["chair"]) : 0; // Fix: Change "kursi" to "chair"
+
+        $filter = [ "id" => $id ];
+        $update = [ '$set' => [
+            "plat" => $plat,
+            "type" => $jenis, // Fix: Change "jenis" to "type"
+            "chair" => $kursi // Fix: Change "kursi" to "chair"
+        ]];
+
+        if ($image["error"] === UPLOAD_ERR_OK) {
+            $tempFilePath = $image["tmp_name"];
+            $extension = pathinfo($image["name"], PATHINFO_EXTENSION);
+            $destinationPath = '../image/busprofile/' . $id . '.' . $extension;
+            move_uploaded_file($tempFilePath, $destinationPath);
+            $update['$set']['image'] = $destinationPath; // Fix: Change "profile" to "image"
+        }
+
+        $result = $collection->updateOne($filter, $update);
+
+        if ($result->getModifiedCount() > 0) {
+            header("Location: ../php/listbus.php");
+            exit();
+        } else {
+            echo "Update Failed";
+        }
+    }
+?>
