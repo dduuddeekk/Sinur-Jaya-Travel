790d8f9c280a848363806224b0fa4038a265fdc3
dduuddeekk <jajajablablabla123@gmail.com>
2023-06-18 11:49:59 +0800

    Search Bar di Bus.

--- php/bus.php
+++ php/bus.php
@@ -1,14 +1,35 @@
 <?php
     include "../php/connect.php";
 
-    $id = $_GET["id"];
+    $id = isset($_GET["id"]) ? $_GET["id"] : "";
 
-    $userCollection = $database->selectCollection("users");
-    $userDocument = $userCollection->findOne(["id" => $id]);
-    $userid = $userDocument["id"];
+    $userDocument = null;
+    $userid = "";
+
+    if (!empty($id)) {
+        $userCollection = $database->selectCollection("users");
+        $userDocument = $userCollection->findOne(["id" => $id]);
+        $userid = $userDocument ? $userDocument["id"] : "";
+    }
 
     $collection = $database->selectCollection("bus");
     $documents = $collection->find();
+
+    $searchKeyword = isset($_GET["search"]) ? $_GET["search"] : "";
+    $filteredDocuments = [];
+
+    if (!empty($searchKeyword)) {
+        $filter = ['$or' => [
+            ["id" => ['$regex' => $searchKeyword, '$options' => 'i']],
+            ["plat" => ['$regex' => $searchKeyword, '$options' => 'i']],
+            ["type" => ['$regex' => $searchKeyword, '$options' => 'i']],
+            ["chair" => ['$regex' => $searchKeyword, '$options' => 'i']]
+        ]];
+
+        $filteredDocuments = $collection->find($filter);
+    } else {
+        $filteredDocuments = $documents;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -22,8 +43,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <title>BUS</title>
     <style>
-        .busnya{
+
+        .busnya {
             background-color: aliceblue; 
+            width: 100%;
+            max-width: 800px;
+            padding: 20px;
+            box-sizing: border-box;
         }
         
         .buslist {
@@ -52,6 +78,34 @@
             object-fit: cover;
             margin-bottom: 10px;
         }
+
+        .searchbar {
+            margin-bottom: 20px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+        }
+
+        .searchbar form {
+            display: flex;
+            align-items: center;
+        }
+
+        .searchbar input[type="text"] {
+            padding: 5px;
+            width: 200px;
+            border: 1px solid #fcd733;
+            border-radius: 4px;
+        }
+
+        .searchbar button {
+            padding: 5px 10px;
+            background-color: #fcd733;
+            color: aliceblue;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
@@ -69,22 +123,25 @@
                     <li><a id="userMain" href="../php/userindex.php">beranda</a></li>
                     <li><a id="userLink" href="../php/user.php">akun</a></li>
                     <li><a id="userBus" href="../php/bus.php">bus</a></li>
-                    <li><a id ="userRoute" href="../php/rute.php">rute</a></li>
-                    
+                    <li><a id="userRoute" href="../php/rute.php">rute</a></li>
                     <script src="../javascript/mainer.js"></script>
                     <script src="../javascript/linker.js"></script>
                     <script src="../javascript/buser.js"></script>
                     <script src="../javascript/router.js"></script>
-
-
                     <li><a id="userTikets" href="../php/tiket.php">tiket</a></li>
                     <script src="../javascript/tiketers.js"></script>
                 </ul>
             </nav>
         </div>
         <div class="busnya">
+            <div class="searchbar">
+                <form action="../php/bus.php" method="GET">
+                    <input type="text" name="search" placeholder="Cari bus" value="<?php echo $searchKeyword; ?>">
+                    <button type="submit">Cari</button>
+                </form>
+            </div>
             <ul class="buslist">
-                <?php foreach ($documents as $document): ?>
+                <?php foreach ($filteredDocuments as $document): ?>
                     <li>
                         <img class="busimage" src="<?php echo $document['image']; ?>" alt="Bus Image">
                         <span>ID: <?php echo $document["id"]; ?></span><br>
@@ -92,9 +149,11 @@
                         <span>Plat: <?php echo $document["plat"]; ?></span><br>
                         <span>Jenis: <?php echo $document["type"]; ?></span><br>
                         <span>Kursi: <?php echo $document["chair"]; ?></span><br>
-                        <form action="../php/formtiket.php?busid=<?php echo $busid; ?>&userid=<?php echo $userid; ?>" method="POST">
-                            <button type="submit">Beli</button>
-                        </form>
+                        <?php if (!empty($userid)): ?>
+                            <form action="../php/formtiket.php?busid=<?php echo $busid; ?>&userid=<?php echo $userid; ?>" method="POST">
+                                <button type="submit">Beli</button>
+                            </form>
+                        <?php endif; ?>
                     </li>
                 <?php endforeach; ?>
             </ul>
